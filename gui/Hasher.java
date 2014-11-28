00ceef39ec58fcbe3a07ead6d5a33dd1d103932d
Ark Eidolon <clarke.plumo@gmail.com>
2014-11-28 14:36:23 +0800

    Create hasher for answers

---
+++ gui/Hasher.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Hasher {
+	public static void main(String[] args) throws Exception {
+		if(args.length != 5) {
+			System.out.print("Incorrect number of arguments.\nUsage: java Hasher <file to be hashed> "+
+				"<output file> <plain line count> <encrypted line cout> <plain line count>");
+			return;
+		}
+		else {
+			BufferedReader br 	= new BufferedReader(new FileReader(new File(args[0])));
+			PrintWriter pw		= new PrintWriter(new FileWriter(new File(args[1])));
+
+			int plainLineCount1 = Integer.parseInt(args[2]);
+			int plainLineCount2 = Integer.parseInt(args[4]);
+			int encrptLineCount = Integer.parseInt(args[3]);
+
+			for(int i = 0; i < plainLineCount1; i++)
+				pw.println(br.readLine());
+			for(int i = 0; i < encrptLineCount; i++)
+				pw.println(hash(br.readLine()));
+			for(int i = 0; i < plainLineCount2; i++)
+				pw.println(br.readLine());
+
+			br.close();
+			pw.close();
+		}
+	}
+	public static int hash(String str) {
+		int hash = 7;
+		for (int i = 0; i < str.length(); i++)
+			hash = hash*31+str.charAt(i);
+
+		return hash;
+	}
+}

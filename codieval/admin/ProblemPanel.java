b65108196a0f13f37b24cdd4d4c2ca3559522981
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-01-03 17:06:49 +0800

    Add info button for problems manager

--- codieval/admin/ProblemPanel.java
+++ codieval/admin/ProblemPanel.java
@@ -6,17 +6,35 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-import codieval.problem.Problem;
+import codieval.problem.*;
 
 public class ProblemPanel extends JPanel {
 
 	public Problem problem;
 
-	public ProblemPanel(Problem problem) {
+	public ProblemPanel(final Problem problem) {
 		super(new FlowLayout(FlowLayout.LEFT));
 
+		final JPanel panel = this;
 		this.problem = problem;
 
+		JButton btnInfo = new JButton("Info");
+
+		btnInfo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(panel);
+				frame.setFocusableWindowState(false);
+
+				try {
+					new Requirements(problem);
+				}
+				catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+				// frame.setFocusableWindowState(true);
+			}
+		});
+		add(btnInfo);
 		add(new Checkbox(problem.toString(), true));
 	}
 }

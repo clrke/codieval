3e8d82649bf9e8203180cd05d237739202056388
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-02-19 11:31:11 +0800

    Make time complexity getter more robust

--- codieval/complexity/Complexity.java
+++ codieval/complexity/Complexity.java
@@ -7,28 +7,94 @@ public class Complexity {
         BigO bigO = new BigO();
 
         int open_bracket=0;
-        StringTokenizer t = new StringTokenizer(code);
+        Lexer lexer = new Lexer(code, new char[] {' ', '\t', '(', ')', '{', '}', ';'});
 
-        String token="";
-        int current = 0;
-
-        while(t.hasMoreTokens())
+        for(int i = 0; i < lexer.size(); i++)
         {
-            token = t.nextToken();
+            String token = lexer.get(i).toString();
+            System.out.println(token);
 
-            if(token.equals("{")) open_bracket++;
-
-            if(token.equals("}")) open_bracket--;
-
-            if(token.length()>=3) if(token.substring(0, 3).equals("for"))  current++;
-
-            if(open_bracket==0&&token.equals("}"))
-            {
-                bigO.add(current);
-                current = 0;
+            if(token.equals("for")) {
+                int[] analysis = depthOfForLoop(lexer, i);
+                int depth = analysis[0];
+                i = analysis[1];
+                bigO.add(depth);
             }
         }
 
         return bigO;
+    }
+
+    public static int[] depthOfForLoop(Lexer lexer, int i) {
+        int depth = 1;
+        int parentheses = 0;
+
+        i += 2;
+        parentheses++;
+
+        for(; i < lexer.size(); i++) {
+            String token = lexer.get(i).toString();
+            System.out.println("for loop! " + token);
+            if(token.equals("(")) {
+                parentheses++;
+            } else if(token.equals(")")) {
+                parentheses--;
+            }
+
+            if(parentheses == 0) {
+                break;
+            }
+        }
+
+        i++;
+        int curlyBraces = 0;
+
+        if(i < lexer.size() && lexer.get(i).toString().equals("{")) {
+            curlyBraces++;
+            i++;
+
+            for(; i < lexer.size(); i++) {
+                String token = lexer.get(i).toString();
+                System.out.println("for loop 2! " + token);
+
+                if(token.equals("{")) {
+                    curlyBraces++;
+                } else if(token.equals("}")) {
+                    curlyBraces--;
+                }
+                else if(token.equals("for")) {
+                    int[] analysis = depthOfForLoop(lexer, i);
+                    int depth2 = analysis[0];
+                    i = analysis[1];
+
+                    if(depth2 + 1 > depth) {
+                        depth = depth2 + 1;
+                    }
+                }
+
+                if(curlyBraces == 0) {
+                    break;
+                }
+            }
+            i++;
+        } else {
+            for(; i < lexer.size(); i++) {
+                String token = lexer.get(i).toString();
+
+                System.out.println("for loop 3! " + token);
+                if(token.equals(";")) {
+                    break;
+                } else if(token.equals("for")) {
+                    int[] analysis = depthOfForLoop(lexer, i);
+                    int depth2 = analysis[0];
+                    i = analysis[1];
+
+                    depth = depth2 + 1;
+
+                    break;
+                }
+            }
+        }
+        return new int[] {depth, i};
     }
 }

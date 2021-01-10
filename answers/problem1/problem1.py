86ea01ec12495775f1b7461466ac2c1ee181dfd3
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2021-01-10 20:16:07 +0800

    Fix problem1.py

--- answers/problem1/problem1.py
+++ answers/problem1/problem1.py
@@ -2,4 +2,4 @@ input = open('input.txt', 'r')
 output = open('output.txt', 'w')
 
 for line in input.readlines():
-    output.write('Hello %s!\n'%line[:-1])
+    output.write('Hello %s!\n'%line[:-2])
